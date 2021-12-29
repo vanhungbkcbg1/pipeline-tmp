21d5f93cc663ea9052650c6b9cf8d03709846fc0
vanhung <vanhungbkcbg1@gmail.com>
2021-12-29 09:42:44 +0700

    script for codedeploy

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Before install"
+echo "After install"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar self-update 1.10.1
+#php composer.phar install --no-interaction --no-progress
--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Before install"
+apt update -y && apt install nginx -y
+apt install php-fpm php-mysql -y
+service php-fpm start
+
+
