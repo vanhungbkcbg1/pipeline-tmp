db105f191a975f3470b9b75cddba432e2084f859
vanhung <vanhungbkcbg1@gmail.com>
2021-12-29 11:26:24 +0700

    script for codedeploy

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -4,7 +4,13 @@ apt install  ca-certificates apt-transport-https software-properties-common -y
 add-apt-repository ppa:ondrej/php -y
 apt update -y
 apt install nginx -y
-apt install php8.0-fpm php9.0-mysql php8.0-mbstring -y
+apt install php8.0-fpm \
+            php8.0-mysql \
+            php8.0-mysql \
+            php8.0-xml \
+            php8.0-curl \
+            php8.0-mbstring -y
+
 service php8.0-fpm start
 
 
