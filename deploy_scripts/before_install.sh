02e96d58c55a1601ddf285813a8db8166d35ae38
vanhung <vanhungbkcbg1@gmail.com>
2021-12-29 10:53:49 +0700

    script for codedeploy

--- deploy_scripts/before_install.sh
+++ deploy_scripts/before_install.sh
@@ -4,7 +4,7 @@ apt install software-properties-common -y
 add-apt-repository ppa:ondrej/php
 apt update -y
 apt install nginx -y
-apt install php7.4-fpm php7.4-mysql -y
+apt install php7.4-fpm php7.4-mysql php7.4-mbstring -y
 service php7.4-fpm start
 
 
