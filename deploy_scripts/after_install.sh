a6fcacf5f1506ee92df9e54a36e0dde7ca5a6a74
vanhung <vanhungbkcbg1@gmail.com>
2021-12-29 11:34:09 +0700

    script for codedeploy

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -4,7 +4,7 @@ cd /var/www/html
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-php composer.phar self-update 1.10.1
+php composer.phar self-update 1.10.10
 php composer.phar install --no-interaction --no-progress
 # copy config to nginx config
 cp ./conf/laravel.conf /etc/nginx/conf.d/
