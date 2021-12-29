fd34174bd42054965540849f3ab6372b471487ce
vanhung <vanhungbkcbg1@gmail.com>
2021-12-29 10:04:29 +0700

    script for codedeploy

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "After install"
+cd /var/www/html
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
