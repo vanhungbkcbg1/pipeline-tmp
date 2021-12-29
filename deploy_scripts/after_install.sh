2b2ba1a679de9d205267ea8983341a9516fa2b64
vanhung <vanhungbkcbg1@gmail.com>
2021-12-29 11:59:37 +0700

    script for codedeploy

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -6,6 +6,9 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 php composer.phar self-update 1.10.10
 php composer.phar install --no-interaction --no-progress
+cp .env.example .env
+php artisan key:generate
+chown -R www-data:www-data /var/www/html/storage/
 # copy config to nginx config
 cp ./conf/laravel.conf /etc/nginx/sites-available/default
 service nginx reload
